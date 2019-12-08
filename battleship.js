93d0c694e1f68cabdafb6857a20901732f0d5961
Zubair <zubair.maqsood6@gmail.com>
2019-12-08 22:43:44 +0000

    js

---
+++ battleship.js
@@ -0,0 +1,12 @@
+let view = {
+	displayMessage: function(message){
+		let messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = message;
+
+	},
+	displayHit: function(location){
+
+	},
+	displayMiss: function(location){},
+
+}
